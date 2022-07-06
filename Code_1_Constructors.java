6ff6cbf7a1f7c3d846d9db8a4e33584d463ac8ff
Swarali-Joglekar-2001 <74260706+Swarali-Joglekar-2001@users.noreply.github.com>
2022-07-07 01:18:43 +0530

    Update Code_1_Constructors.java

--- Code_1_Constructors.java
+++ Code_1_Constructors.java
@@ -1,19 +1,19 @@
 package SwaraliPractice;
 
-class stud{
+class stu{
     String name;
     int age;
 
-    stud(){   // non-parameterised
+    stu(){   // non-parameterised
 
         //System.out.println("constructor called");
     }
-    stud(String name, int age){  // parameterised constructor
+    stu(String name, int age){  // parameterised constructor
         this.name=name;
         this.age=age;
     }
 
-    stud(stud s){ // copy constructor
+    stu(stu s){ // copy constructor
         this.name= s.name;
         this.age=s.age;
        // this.age=15;
@@ -31,13 +31,13 @@ class stud{
 
 public class Code_1_Constructors {
     public static void main(String[] args) {
-            stud s1 = new stud("XYZ",20);
+            stu s1 = new stu("XYZ",20);
             s1.printInfo();
 
-            stud s2 = new stud(s1);
+            stu s2 = new stu(s1);
             s2.printInfo();
 
-            stud s3= new stud();
+            stu s3= new stu();
             s3.name="Jim";
             s3.age=27;
             s3.printInfo();

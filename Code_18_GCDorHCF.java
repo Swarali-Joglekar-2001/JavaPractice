9dcca0fcc0acb944c02fac39c2f6a8cde1eb99cd
Swarali-Joglekar-2001 <74260706+Swarali-Joglekar-2001@users.noreply.github.com>
2022-08-15 13:32:02 +0530

    Update Code_18_GCDorHCF.java

--- Code_18_GCDorHCF.java
+++ Code_18_GCDorHCF.java
@@ -7,7 +7,7 @@ import java.io.*;
 
 class Comp
 {
-	public static void main (String[] args) throws java.lang.Exception
+	 public static void main (String[] args) throws java.lang.Exception
 	{
 		// Greatest common divisor of 2 numbers
 		
@@ -17,16 +17,21 @@ class Comp
 		n2=sc.nextInt();
 		
 		int hcf=1;
-		
+		int flag=0;
 		for(int i=(n1>n2)?n2:n1 ; i>=2; i--){
 		    if(n1%i==0 && n2%i==0){
 		        hcf=i;
+		        flag=1;
 		        break;
 		    }
 		        
 		}
+		if(flag==0) // no common factor except 1
+		  System.out.print(1);
 		
-		System.out.print(hcf);
+		else
+		 System.out.print(hcf);
 		
 	}
+    
 }
